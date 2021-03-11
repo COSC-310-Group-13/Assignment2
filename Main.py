8e8e653e878a802f946f81b37ce46d7ac157f3ff
Sebi Unipan <onebread@student.ubc.ca>
2021-03-11 14:44:05 -0800

    Refining code

---
+++ Main.py
@@ -0,0 +1,20 @@
+from ChatBot import ChatBot
+
+#Main class where the bot will be run from.
+
+cb = ChatBot()
+cb.extractQuotes('quotes.txt') #we establish the quotes in the object
+
+print("Calm Bot: Hello, my name is Calm Bot and I'm here to help you!")
+
+exitWords = ['bye','quit','exit','see ya','good bye'] #Exit the chat bot with common greetings
+
+while(True):
+    userInput = input()
+    if userInput.lower() in exitWords:
+        print("It was really nice talking to you!")
+    else:
+        if cb.helloMessage(userInput) != None:
+            print("Calm Bot: " + cb.helloMessage(userInput))
+        else:
+            print("Calm Bot: " + cb.botResponse(userInput))
